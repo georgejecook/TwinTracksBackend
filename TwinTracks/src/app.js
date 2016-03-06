f4eb3e0fa5c439a4509aeee835f340d9594e02b0
George Cook <george.cook@twintechs.com>
2016-03-05 22:05:03 -0500

    some files that were missed

--- TwinTracks/src/app.js
+++ TwinTracks/src/app.js
@@ -1,9 +1,8 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+import path from 'path';
+import http from 'http';
+import express from 'express';
+import bodyParser from 'body-parser';
+import api from './api';
 require("babel-core/register");
 
 
